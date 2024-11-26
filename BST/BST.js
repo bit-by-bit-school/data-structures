398e26d074059a8a04ac589548717bc488620e75
archita-b <archita.bhattacharyya91@gmail.com>
2024-11-26 17:53:14 +0530

    find, insert method in BST

---
+++ BST/BST.js
@@ -0,0 +1,66 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  find(value) {
+    let currentNode = this.root;
+
+    while (currentNode != null) {
+      if (value === currentNode.value) return currentNode;
+
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
+    return null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (this.root == null) {
+      this.root = newNode;
+      return;
+    }
+
+    let currentNode = this.root;
+    while (true) {
+      if (value < currentNode.value) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          return;
+        }
+        currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return;
+        }
+        currentNode = currentNode.right;
+      }
+    }
+  }
+}
+
+const bst = new BinarySearchTree();
+
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(7);
+
+console.log(bst);
+console.log("Search for 7:", bst.find(7));
+console.log("Search for 20:", bst.find(20));
