a121ec954b606fb3984a6e7cf20a13db6de97cb4
archita-b <archita.bhattacharyya91@gmail.com>
2024-12-05 23:18:28 +0530

    getChildren and iterator method

--- quadTree/quadTree.js
+++ quadTree/quadTree.js
@@ -84,6 +84,69 @@ class QuadTree {
       point.y <= this.bottomRight.y
     );
   }
+
+  getChildren() {
+    const children = [];
+    const quadrants = ["topLeft", "bottomLeft", "topRight", "bottomRight"];
+
+    for (const quadrant of quadrants) {
+      const child = this.subTrees[quadrant]; // here node is a point, whose children I'm supposed to find
+      if (child != null) {
+        children.push(child);
+      }
+    }
+    return children;
+  }
+
+  // eager algo
+  // [Symbol.iterator]() {
+  //   const queue = [];
+
+  //   const populateQueue = function (tree) {
+  //     const children = tree.getChildren();
+
+  //     for (const child of children) {
+  //       if (child instanceof Node) {
+  //         queue.push(child.value);
+  //       } else if (child instanceof QuadTree) {
+  //         populateQueue(child);
+  //       }
+  //     }
+  //   };
+  //   populateQueue(this); // calculates all values beforehand
+
+  //   return {
+  //     next: () => {
+  //       if (queue.length === 0) return { done: true };
+
+  //       return { value: queue.shift(), done: false };
+  //     },
+  //   };
+  // }
+
+  // lazy algo
+  [Symbol.iterator]() {
+    const queue = [this];
+
+    return {
+      next: () => {
+        while (queue.length > 0) {
+          const current = queue.shift();
+
+          if (current instanceof QuadTree) {
+            const children = current.getChildren();
+
+            for (const child of children) {
+              queue.push(child);
+            }
+          } else if (current instanceof Node) {
+            return { value: current.value, done: false };
+          }
+        }
+        return { done: true };
+      },
+    };
+  }
 }
 
 const quad = new QuadTree(new Point(0, 0), new Point(8, 8));
@@ -95,4 +158,5 @@ quad.insert(a);
 quad.insert(b);
 quad.insert(c);
 quad.insert(d);
-console.log(quad);
+// const quad1 = new QuadTree(new Point(0, 0), new Point(4, 4));
+console.log([...quad]);
